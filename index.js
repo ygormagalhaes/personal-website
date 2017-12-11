2bcad7de3c15b59f21b56a7e1f97695dc1ca37e3
Ygor Magalhães <emaildoygor@gmail.com>
2017-12-11 15:57:44 -0200

    Ajustes para teste com nova entidade

--- index.js
+++ index.js
@@ -4,18 +4,15 @@ var apiEndpoint = "https://ygor-website.prismic.io/api/v2";
 
 window.onload = function() {
     Prismic.getApi(apiEndpoint).then(function(api) {
-      return api.query(Prismic.Predicates.at('document.type', 'cliente')); // An empty query will return all the documents
+      return api.query(Prismic.Predicates.at('document.type', 'post'),
+      { orderings : '[my.last_publication_date desc]' }); // An empty query will return all the documents
+
     }).then(function(response) {
-      console.log("Documents: ", response.results);
+      console.log("Posts: ", response.results);
 
       response.results.forEach((result) => {
 
-        let cliente = result.data;
-        console.log(cliente.nome[0].text);
-        console.log(cliente.descricao[0].text);
-        console.log(cliente.telefone);
-        console.log(cliente.imagem.url);
-        console.log(cliente.apelido);
+        renderizaPost(result.data);
 
       });
 
@@ -23,3 +20,23 @@ window.onload = function() {
       console.log("Something went wrong: ", err);
     });
  };
+
+function renderizaPost(blogPost) {
+  console.log(blogPost);
+
+  let titulo = $('<h1>');
+  titulo.text(blogPost.titulo[0].text);
+
+  let corpo = $('<p>');
+  corpo.text(blogPost.texto[0].text);
+
+
+  let imagem = $('<img>');
+  if (typeof blogPost.imgprincipal != undefined) {
+    imagem.attr('src', blogPost.imgprincipal.url);
+  }
+
+  $('body').append(titulo);
+  $('body').append(corpo);
+  $('body').append(imagem);
+}
--- super.index.js
+++ super.index.js
@@ -5,18 +5,15 @@ var apiEndpoint = "https://ygor-website.prismic.io/api/v2";
 
 window.onload = function() {
     Prismic.getApi(apiEndpoint).then(function(api) {
-      return api.query(Prismic.Predicates.at('document.type', 'cliente')); // An empty query will return all the documents
+      return api.query(Prismic.Predicates.at('document.type', 'post'),
+      { orderings : '[my.last_publication_date desc]' }); // An empty query will return all the documents
+
     }).then(function(response) {
-      console.log("Documents: ", response.results);
+      console.log("Posts: ", response.results);
 
       response.results.forEach((result) => {
 
-        let cliente = result.data;
-        console.log(cliente.nome[0].text);
-        console.log(cliente.descricao[0].text);
-        console.log(cliente.telefone);
-        console.log(cliente.imagem.url);
-        console.log(cliente.apelido);
+        renderizaPost(result.data);
 
       });
 
@@ -25,6 +22,25 @@ window.onload = function() {
     });
  };
 
+function renderizaPost(blogPost) {
+  console.log(blogPost);
+
+  let titulo = $('<h1>');
+  titulo.text(blogPost.titulo[0].text);
+
+  let corpo = $('<p>');
+  corpo.text(blogPost.texto[0].text);
+
+
+  let imagem = $('<img>');
+  if (typeof blogPost.imgprincipal != undefined) {
+    imagem.attr('src', blogPost.imgprincipal.url);
+  }
+
+  $('body').append(titulo);
+  $('body').append(corpo);
+  $('body').append(imagem);
+}
 },{"prismic-javascript":3}],2:[function(require,module,exports){
 // the whatwg-fetch polyfill installs the fetch() function
 // on the global object (window or self)
